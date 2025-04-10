724a0171fe193878ac745d43e456c743c43bae67
Jaden Szewczak <jaden.szewczak@wakefern.com>
2025-04-10 19:14:31 -0400

    Fix client-side routing for Render deployment

--- server/src/server.ts
+++ server/src/server.ts
@@ -16,6 +16,12 @@ app.use(express.static('../client/dist'));
 app.use(express.json());
 app.use(routes);
 
+// catch-all route after other routes and before starting the server
+// using an underscore for the unused parameter
+app.get('*', (_req, res) => {
+  res.sendFile('index.html', { root: '../client/dist' });
+});
+
 sequelize.sync({force: forceDatabaseRefresh}).then(() => {
   app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
