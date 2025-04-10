b682af9dafeefb0dc45c094ace7c7b7cd4b42028
Jaden Szewczak <jaden.szewczak@wakefern.com>
2025-04-10 19:17:35 -0400

    Fix client file paths for Render deployment

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,28 +1,37 @@
 const forceDatabaseRefresh = false;
 
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
 
-import express from 'express';
-import sequelize from './config/connection.js';
-import routes from './routes/index.js';
+import express from "express";
+import sequelize from "./config/connection.js";
+import routes from "./routes/index.js";
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 // Serves static files in the entire client's dist folder
-app.use(express.static('../client/dist'));
+app.use(
+  express.static(
+    process.env.NODE_ENV === "production" ? "./client/dist" : "../client/dist"
+  )
+);
 
 app.use(express.json());
 app.use(routes);
 
 // catch-all route after other routes and before starting the server
 // using an underscore for the unused parameter
-app.get('*', (_req, res) => {
-  res.sendFile('index.html', { root: '../client/dist' });
+app.get("*", (_req, res) => {
+  res.sendFile("index.html", {
+    root:
+      process.env.NODE_ENV === "production"
+        ? "./client/dist"
+        : "../client/dist",
+  });
 });
 
-sequelize.sync({force: forceDatabaseRefresh}).then(() => {
+sequelize.sync({ force: forceDatabaseRefresh }).then(() => {
   app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
   });
